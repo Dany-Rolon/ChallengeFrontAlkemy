61c56f633f7eaebea9d7607d80b9bec079cdf698
Dany-Rolon <dany_rolon@outlook.com>
2021-06-17 19:34:13 -0300

    Add post creation view and App ready for test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { Switch, Route } from 'react-router-dom'
+import Navbar  from './components/Navbar'
+
+import FormularioDeCreacion from './view/FormularioDeCreacion'
 
 function App() {
   return (
     <div className="">
-      Hola Mundo
+      <Navbar />
+      <Switch>
+        {/* <Route exact path="/" component={Home} /> */}
+        {/* <Route exact path="/detalle" component={Detalle} /> */}
+        <Route exact path="/formulariodecreacion" component={FormularioDeCreacion} />
+        {/* <Route exact path="/formulariodeedicion" component={FormularioDeEdicion} /> */}
+        {/* <Route exact path="/eliminar/:id/:titulo" component={Eliminar} /> */}
+      </Switch>
     </div>
   );
 }
 
-export default App;
+export default App
