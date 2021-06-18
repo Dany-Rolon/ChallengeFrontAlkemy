109a94547145f6b2cb7833577f9ff98f40993608
Dany-Rolon <dany_rolon@outlook.com>
2021-06-18 17:45:15 -0300

    feat: Add Post Component

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,15 +1,18 @@
-import Axios from 'axios'
+import {Link} from 'react-router-dom'
 
-const Posts = async () => {
-    let response = await Axios.get('https://jsonplaceholder.typicode.com/posts')
-    console.log(response)
-    return ( 
-        <div>
-            
+const Posts = ({ post }) => {
+    return (
+        <div className="w-full">
+            <div className="border-2 p-2 m-2 rounded-md shadow-lg">
+                <h1 className="text-center text-md tituloPost">{post.title}</h1>
+                <div className="flex mt-2">
+                    <Link to={`/detalle/${post.id}`} className="text-center text-blue-500 w-full rounded-md border-2 border-blue-500 m-1 font-semibold hover:bg-blue-700 hover:text-white">Detalle</Link>
+                    <Link to={`/formulariodeedicion/${post.id}$`} className="text-center text-yellow-700 w-full rounded-md border-2 border-yellow-700 m-1 font-semibold hover:bg-yellow-700 hover:text-white">Modificar</Link>
+                    <Link to={`/eliminar/${post.id}/${post.title}`} className="text-center text-red-400 w-full rounded-md border-2 border-red-400 m-1 font-semibold hover:bg-red-700 hover:text-white">Eliminar</Link>
+                </div>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default Posts;
-
-
