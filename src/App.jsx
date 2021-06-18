1fbd65e5f1bebbb8484953e520254cd5bf358051
Dany-Rolon <dany_rolon@outlook.com>
2021-06-18 17:43:49 -0300

    feat: Add routes for details, creation and delete posts

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import { Switch, Route } from 'react-router-dom'
 import Navbar  from './components/Navbar'
 
 import FormularioDeCreacion from './view/FormularioDeCreacion'
+import Home from './view/Home'
+import Detalles from './view/Detalles'
+import Eliminar from './view/Eliminar'
+import FormularioDeEdicion from './view/FormularioEdicion'
 
 function App() {
   return (
     <div className="">
       <Navbar />
       <Switch>
-        {/* <Route exact path="/" component={Home} /> */}
-        {/* <Route exact path="/detalle" component={Detalle} /> */}
+        <Route exact path="/" component={Home} />
+        <Route exact path="/detalle/:id" component={Detalles} />
         <Route exact path="/formulariodecreacion" component={FormularioDeCreacion} />
-        {/* <Route exact path="/formulariodeedicion" component={FormularioDeEdicion} /> */}
-        {/* <Route exact path="/eliminar/:id/:titulo" component={Eliminar} /> */}
+        <Route exact path="/formulariodeedicion/:id" component={FormularioDeEdicion} />
+        <Route exact path="/eliminar/:id/:titulo" component={Eliminar} />
       </Switch>
     </div>
   );
