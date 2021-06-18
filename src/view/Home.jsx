746080121bec51d84a57d7f93b26174b39a73605
Dany-Rolon <dany_rolon@outlook.com>
2021-06-18 17:47:49 -0300

    Update Home.jsx

--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -1,9 +1,26 @@
 import Posts from './../components/Posts'
+import {GetPosts} from '../helpers/PostsHelpers'
+import { useEffect, useState } from 'react'
 
 const Home = () => {
+
+    const [posts, setPosts] = useState([
+        {title: ''}
+    ])
+
+    useEffect(() => {
+        const GetPostsFromApi = async () => {
+            let allThePosts = await GetPosts()
+            setPosts(allThePosts)   
+        }
+
+        GetPostsFromApi()
+    }, [])
+
+
     return ( 
-        <div>
-            
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 justify-items-center place-content-center">
+            {posts.map(post => <Posts key={post.id} post={post} />)}
         </div>
      );
 }
