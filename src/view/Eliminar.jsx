ba7cd32fe765681d6cab3a73d0e76f26ed4772dc
Dany-Rolon <dany_rolon@outlook.com>
2021-06-19 18:02:46 -0300

    Replace cancel button link for a history function

--- src/view/Eliminar.jsx
+++ src/view/Eliminar.jsx
@@ -1,5 +1,5 @@
 import { DeletePost } from '../helpers/PostsHelpers'
-import {Link, useHistory} from 'react-router-dom'
+import {useHistory} from 'react-router-dom'
 
 export default function Eliminar(props){
     const history = useHistory()
@@ -11,12 +11,16 @@ export default function Eliminar(props){
         history.replace('/')
     }
 
+    async function OnCancel(){
+        history.replace('/')
+    }
+
     return(
         <div className="grid grid-cols-1 mt-40 justify-items-center place-content-center">
             <div className="border shadow-lg p-2 rounded-md mx-4">
                 <h3>¿Seguro que quieres eliminar el post <b>{titulo}</b>?</h3>
                 <div className="flex w-full mt-2 justify-evenly">
-                    <Link to="/" className="text-center rounded-md w-full font-semibold border-2 border-blue-500 p-1 m-2 hover:bg-blue-500 hover:text-white">Cancelar</Link>
+                    <button onClick={OnCancel} className="rounded-md w-full font-semibold border-2 border-blue-500 p-1 m-2 hover:bg-blue-500 hover:text-white">Cancelar</button>
                     <button onClick={OnDelete} className="rounded-md w-full font-semibold border-2 border-red-500 p-1 m-2 hover:bg-red-500 hover:text-white">Eliminar</button>
                 </div>
             </div>
